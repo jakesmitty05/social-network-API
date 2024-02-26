2fa33ae89e0550f3c1dec657252a2c617c854666
Jakesmitty05 <jakesmitty05@gmail.com>
2024-02-25 20:36:32 -0500

    added routes to user and thought routes,js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+      .get(getOneThought)
+      .put(updateThought)
+      .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
